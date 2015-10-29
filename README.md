a6b121e04978e36e03b658e40f43f8d0256e8c96
Ubuntu <ubuntu@ip-172-31-8-194.ec2.internal>
2015-10-29 09:51:16 +0000

    Added documentation

---
+++ README.md
@@ -0,0 +1,12 @@
+
+This is small piece of code to demonstrate how 
+part-of-speech tags can be used to detect a
+particular type of tokens in given text and can be annonymized
+using a masking string. In given example here, we are masking
+credit card numbers in an email. 
+
+
+Apart from dependencies, a English corpora also needed to
+be installed, using following command: 
+
+	$ python -m spacy.en.download all
--- demo.py
+++ demo.py
@@ -9,6 +9,12 @@ nlp = English(data_dir=data_dir)
 
 
 def annonymizer(input_line):
+	"""
+	Takes a input string and checkes for
+	some specific pos tag(e.g. 'NN' or 'CD')
+	and replaces the corresoponding token of
+	that tag with some mask string.
+	"""
 
 	tokens = nlp(input_line)
 	tags = [tok.tag_ for tok in tokens]
@@ -16,11 +22,15 @@ def annonymizer(input_line):
 		if tag == 'CD':
 			input_line = re.sub(str(tok),'XXXX', input_line)
 
-	return input_line
+	return input_line, tags
 
 
 some_email = open('sample_email.txt','r').read().splitlines()
 some_email = map(lambda line: unicode(line, errors='ignore'), some_email)
 for sent in some_email:
-	anon_sent = annonymizer(sent)
+	anon_sent, tags = annonymizer(sent)
 	print(anon_sent)
+
+	# One can also prints tags to see which 
+	# kind of tags corresponds to input sentence 
+	print(tags)
